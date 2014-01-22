13978e9da0a0cd2ccc0df257caf57d99c6a213a0
Nate Kidwell <nate@slideseed.com>
2014-01-22 12:45:07 -0500

    up

--- TrainingBulletins.mk
+++ TrainingBulletins.mk
@@ -30,6 +30,13 @@ Training Bulletins Arson sec 1.2.5
 **DON'T CONFUSE WITH...IGNITION DEVICE: MECHANICAL, CHEMICAL, OR ELECTRICAL MEANS USED TO INITIATE A FIRE.
 TB ARSON SEC 1.2.7
 
+WHAT MOST CORRECTLY DEFINES A "MECHANICAL, CHEMICAL, OR ELECTRICAL MEANS USED TO INITIATE A FIRE?"
+Plant
+Trailer
+-- Ignition Device
+Accelerant
+TB ARSON 1.2.7
+
 ALL OF THE FOLLOWING ARE POSSIBLE INDICATORS OF THE USE OF FLAMMABLE ACCELERANTS EXCEPT IN WHICH CHOICE?
 Charring of the floor or low burn marks on the wall or under the door or on the lower part of the door.
 Char in broken patterns or "puddle-like" circle burns on the floor.
@@ -37,6 +44,13 @@ Low burning and deep charring.
 -- Small body of fire with little contents.
 D) HEAVY BODY OF FIRE WITH LITTLE CONTENTS.
 TB ARSON 3.7
+
+AT FATAL FIRE AND ARSON INVESTIGATIONS, SECURITY OF THE INCIDENT SCENE IS OF THE UTMOST IMPORTANCE. EVIDENCE MUST BE DOCUMENTED, COLLECTED AND PRESERVED. DEPENDING ON THE INCIDENT, THE INVESTIGATION MAY EXTEND OVER A PERIOD OF TIME. IN THESE SITUATIONS, A FIRE LINE MUST BE ESTABLISHED AND RELATIVES, NEIGHBORS, SPECTATORS, THE MEDIA AND MEMBERS OF THE DEPARTMENT SHALL NOT CROSS ESTABLISHED FIRE LINES UNLESS AUTHORIZED BY?
+any Fire Marshal
+the Incident Commander
+the assigned Fire Marshal and/or Chief of Department
+-- the assigned Fire Marshal and/or Chief of Operations
+TB ARSON 4.1
 
 ACCORDING TO THE FDNY ARSON TRAINING BULLETIN, A DECEASED VICTIM WITH CO IN HIS BLOOD INDICATES THAT THE VICTIM WAS ALIVE AT THE TIME OF THE FIRE. WHAT COLOR WOULD THE VICTIM APPEAR IF HE HAD CO IN HIS BLOOD?
 International orange
@@ -53,6 +67,14 @@ Usually starts three to four hours after death and is completed after six to eig
 TRAINING BULLETIN ARSON SEC 4.5
 USUALLY STARTS ONE TO TWO HOURS AFTER DEATH AND IS COMPLETED AFTER THREE TO FOUR HOURS AND COULD INDICATE THAT THE VICTIM WAS MOVED AFTER DEATH AND PLACED AT THE FIRE SCENE.
 
+CONTAMINATION IS SELDOM A PROBLEM ASSOCIATED WITH THE _______ OF EVIDENCE.
+-- Collection
+Packaging
+Preservation
+Exchange
+"CONTAMINATION IS SELDOM A PROBLEM ASSOCIATED WITH THE COLLECTION OF EVIDENCE, BUT CAN BECOME A PROBLEM IN RELATION TO ITS PACKAGING AND PRESERVATION."
+TB ARSON 5.2
+
 WHICH OF THE FOLLOWING STATEMENTS REGARDING FIRE TRAVEL AND POINT OF ORIGIN IS NOT IN ACCORD WITH FDNY PROCEDURES?
 Depth of char may indicate the amount of time burned.
 -- Larger blister type charring may indicate longer burning and hot fires.
@@ -60,3 +82,19 @@ Deepest char may be found at the point of origin
 Wood structural members may burn at approximately one inch every 45 minutes.
 TRAINING BULLETIN ARSON 6.8
 SMALLER BLISTER TYPE CHARRING MAY INDICATE LONGER BURNING AND HOT FIRES.
+
+MEMBERS SHOULD BE AWARE THAT ALL OF THE FOLLOWING INDICATE THE POSSIBILITY OF ARSON EXCEPT IN WHICH CHOICE?
+Ransacked conditions of an occupancy
+Any remains of trailers and incendiary devices or unusual residues
+Absence of furniture in an occupancy reported to have been occupied
+-- Evidence of the fire dropping down through tongue and groove flooring, which is always a significant indication
+D) THIS IS NOT ALWAYS SIGNIFICANT, HOWEVER, IT IS INCLUDED IN THIS LISTING AS IT IS A FACTOR
+TB ARSON 7.1
+
+AT A COMMERCIAL FIRE, THE FIRE MARSHAL WILL PERFORM ALL OF THE FOLLOWING UPON HIS/HER ARRIVAL EXCEPT?
+Report to the officer in command of the fire and obtain his evaluation and reasons for declaring the fire of suspicious origin
+Remove physical evidence for lab analysis when necessary
+-- Call for the Photo Unit in all cases
+Make a physical examination
+C) Call for THE PHOTO UNIT, IF HE/SHE DEEMS IT NECESSARY
+TB ARSON 8.6
